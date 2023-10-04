e519e268bacf8b3a50aa4cd7a8f7b0ceffdf9aef
Neha Udeshi <nehaudeshi@Nehas-Air.attlocal.net>
2023-10-04 09:48:14 -0500

    luna

---
+++ VariableWarmup/src/main/java/com/warmup/Variable.java
@@ -0,0 +1,13 @@
+package com.warmup;
+
+public class Variable {
+    public static void main(String[] args) {
+        String personName = "John Doe";
+        int personAge = 25;
+        double productPrice = 19.99;
+        float temperatureCelsius = 27.5f;
+        boolean isLoggedIn = true;
+
+
+    }
+}
